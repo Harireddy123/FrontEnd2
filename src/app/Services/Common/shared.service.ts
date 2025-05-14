b409a0d7e69ea7952a16eb3d860ef7bb05d76316
Harireddy123 <hariprasadreddy413381@gmail.com>
2025-05-14 11:03:12 +0530

    [Hari]:Added :some chages to ui

--- src/app/Services/Common/shared.service.ts
+++ src/app/Services/Common/shared.service.ts
@@ -3,37 +3,21 @@ import { BehaviorSubject } from 'rxjs';
 import { CartService } from '../Cart/cart.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-
   private searchQuerySubject = new BehaviorSubject<string>('');
   searchQuery$ = this.searchQuerySubject.asObservable();
 
   private cartItems: any[] = [];
- 
+
   private cartCount = new BehaviorSubject<number>(0);
   cartCount$ = this.cartCount.asObservable();
 
-// private cartRefreshTrigger=new BehaviorSubject<void>(undefined)
-// cartRefresh$=this.cartRefreshTrigger.asObservable();
-
-  constructor(private cartService:CartService){}
+  constructor(private cartService: CartService) {}
   setSearchQuery(query: string) {
     this.searchQuerySubject.next(query);
   }
-
-
-  //cartdetails
-
-
-// triggerCartRefresh(){
-//   this.cartRefreshTrigger.next();
-// }
-
-
-
-
 
   updateCartCountFromBackend() {
     this.cartService.getCart().subscribe({
@@ -43,10 +27,7 @@ export class SharedService {
       },
       error: () => {
         this.cartCount.next(0); // fallback
-      }
+      },
     });
-
   }
-
- 
 }
